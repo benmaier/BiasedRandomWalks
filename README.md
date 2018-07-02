eeae86e8b9bba45f0577e3c6a090e43d54bb216a
Benjamin Maier <benjaminfrankmaier@gmail.com>
2018-07-02 17:26:18 -0600

    initial

---
+++ README.md
@@ -0,0 +1,10 @@
+# BiasedRandomWalks
+
+Provides classes to sim
+
+## Install 
+
+    $ git clone git@github.com:benmaier/BiasedRandomWalks.git 
+    $ pip install ./BiasedRandomWalks
+
+
---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(name='BiasedRandomWalks',
+      version='0.0.1',
+      description='Provides classes around biased random walks on networks.',
+      url='https://www.github.com/benmaier/BiasedRandomWalks',
+      author='Benjamin F. Maier',
+      author_email='bfmaier@physik.hu-berlin.de',
+      license='MIT',
+      packages=['BiasedRandomWalks'],
+      install_requires=[
+          'numpy>=1.14',
+          'networkx>=2',
+      ],
+      zip_safe=False)
